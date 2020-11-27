0a21ed20d0d38cd25052017b32333710d48adcb6
Mikita Mironenka <m.mironenka@gmail.com>
2020-11-27 10:16:41 +0300

    create labserver repo

---
+++ src/main/java/by/miro/labserver/LabserverApplication.java
@@ -0,0 +1,15 @@
+package by.miro.labserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class LabserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/miro/labserver/LabserverApplicationTests.java
@@ -0,0 +1,13 @@
+package by.miro.labserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
